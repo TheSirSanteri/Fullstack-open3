c6d71a126c5214f3b88585b2b9e7da2b35428dfc
Santeri Hildén <santeri.hilden@aalto.fi>
2025-01-29 16:55:51 +0200

    Korjattu portti

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.post('/api/persons', (request, response) => {
     response.status(201).json(newContact);
 })
   
-const PORT = 3001
-    app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
